727f2eefd23f715530586a6e8095e662194a2fad
happyMing <339755551@qq.com>
2015-08-08 00:34:19 +0800

    创建登录名，migration报错

--- db/migrate/20150807162126_add_login_name_to_users.rb
+++ db/migrate/20150807162126_add_login_name_to_users.rb
@@ -1,4 +1,4 @@
-class AddLoginnameToUsers < ActiveRecord::Migration
+class AddLoginNameToUsers < ActiveRecord::Migration
   def change
     add_column :users, :login_name, :string
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150807082319) do
+ActiveRecord::Schema.define(version: 20150807162126) do
 
   create_table "articles", force: true do |t|
     t.string   "title",                         comment: "标题"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20150807082319) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "login_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
